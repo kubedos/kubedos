d062afa8190f10ec09e454207e1e649d9aaffc73
anthony <sysop@foundrybot.ca>
2026-01-02 13:38:33 -0800

    update: 2026-01-02 13:38:33

--- payload/darksite/bak/artifacts/join_cp.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-kubeadm join k8s-api.unixbox.net:6443 --token v7550c.imtoszwvofielnud --discovery-token-ca-cert-hash sha256:ea74f8b59efa860765513fea3da9964138926aa6fef31199989798ed4870a137  \
-  --control-plane \
-  --certificate-key 9199c1acf21a806d1c7b944b9b069482d0af104744dd261c2a84d2cf0db678e8 \
-  --node-name "${HOSTNAME}" \
-  --apiserver-advertise-address "10.100.10.219"
--- payload/darksite/bak/artifacts/join_worker.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-kubeadm join k8s-api.unixbox.net:6443 --token y87b4v.20uqkatpjp4hrrkf --discovery-token-ca-cert-hash sha256:ea74f8b59efa860765513fea3da9964138926aa6fef31199989798ed4870a137  --node-name "${HOSTNAME}"
